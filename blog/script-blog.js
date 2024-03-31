a380feabba183d1ddfd45a22e1c795b32bdcad1e
Steef Broeder <steef@broeder.co.nl>
2024-03-31 23:03:32 +0200

    post2!!!

--- blog/script-blog.js
+++ blog/script-blog.js
@@ -1,4 +1,9 @@
 document.getElementById('post1')
-              .addEventListener('click', function () {
-                window.location.href = 'post1/post1.html';
-        });
+  .addEventListener('click', function () {
+    window.location.href = 'post1/post1.html';
+  });
+
+document.getElementById('post2')
+  .addEventListener('click', function () {
+    window.location.href = 'post2/post2.html';
+  });

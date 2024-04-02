d191d196d4a1a6bf8801a11d6516b77dba20aa34
Steef Broeder <steef@broeder.co.nl>
2024-04-02 17:38:37 +0200

    fix for thinner screens

--- blog/post4/assets/script.js
+++ blog/post4/assets/script.js
@@ -4,20 +4,20 @@ document.querySelector("h1").onmouseover = event => {
     let iterations = 0;
 
     const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("")
-    .map((letter, index) => {
-        if(index < iterations) {
-            return event.target.dataset.value[index];
-        }
+        event.target.innerText = event.target.dataset.value.split("")
+        .map((letter, index) => {
+            if (event.target.dataset.value[index] === " ") return " "; // Corrected condition for space
+            if (index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            return characters[Math.floor(Math.random() * characters.length)];
+        })
+        .join("");
 
-        return characters[Math.floor(Math.random() * characters.length)];
-    })
-    .join("");
+        if (iterations > event.target.dataset.value.length) {
+            clearInterval(interval);
+        } 
 
-    if (iterations > event.target.dataset.value.length) {
-        clearInterval(interval);
-    } 
-
-    iterations += 1/3;
+        iterations += 1/3;
     }, 30);
 }
